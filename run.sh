af8cd2473ee1511f6c5674f8a5e043d1b8301ccf
Maximilian H Abitbol <mabitbol15@gmail.com>
2021-02-09 16:56:10 +0000

    hybrid method data preparation for BBPower

---
+++ run.sh
@@ -0,0 +1,3 @@
+addqueue -q berg -c "1 week" -m 6 -s -n 1x8 /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe final_runs2/
+addqueue -q cmb -c "1 week" -m 3 -s -n 1x8 /mnt/zfsusers/mabitbol/.local/lib/python3.6/site-packages/bbpipe final_runs2/
+addqueue -q cmb -c "1 week" -m 6 /usr/bin/python3
