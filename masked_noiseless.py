620f17e10364bc830a37ca2848dbddbeeee62da7
Maximilian H Abitbol <mabitbol15@gmail.com>
2021-03-08 16:14:06 +0000

    unifying

--- masked_noiseless.py
+++ masked_noiseless.py
@@ -9,16 +9,22 @@ import bfore.skymodel as sky
 import bfore.instrumentmodel as ins
 from bfore.sampling import clean_pixels, run_emcee, run_minimize, run_fisher
 
+masked = True
 std = 3
 nside = 256
 fdir = '/mnt/zfsusers/mabitbol/simdata/'
 ddir = f'sims_gauss_fullsky_ns256_csd_std0.{std}_gm3/'
 sdir = f'./data/sim0{std}/'
 
-fnames = glob.glob(fdir+ddir+'s*/')
+fnames = glob.glob(f'{fdir}{ddir}s*/')
 fnames.sort()
 
-sat_mask = hp.ud_grade(hp.read_map('/mnt/extraspace/damonge/SO/BBPipe/mask_apodized.fits'), nside)
+if masked:
+    sname = 'masked'
+    sat_mask = hp.ud_grade(hp.read_map('/mnt/extraspace/damonge/SO/BBPipe/mask_apodized.fits'), nside)
+else:
+    sname = 'full'
+    sat_mask = np.ones(hp.nside2npix(nside))
 npix = np.sum(sat_mask>0)
 
 def clean_maps(k, fn):
@@ -79,8 +85,8 @@ def clean_maps(k, fn):
     filled_maps = np.zeros((12, hp.nside2npix(nside))) 
     filled_maps[:, sat_mask>0] = reducedmaps
     
-    np.savez(f'{sdir}masked_hybrid_params{k}', params=rdict['params_ML'], Sbar=Sbar, Q=Q)
-    hp.write_map(f'{sdir}masked_residualmaps{k}.fits', filled_maps, overwrite=True)
+    np.savez(f'{sdir}{sname}_hybrid_params{k}', params=rdict['params_ML'], Sbar=Sbar, Q=Q)
+    hp.write_map(f'{sdir}{sname}_residualmaps{k}.fits', filled_maps, overwrite=True)
     return
 
 
